ca1902afa26cacc8ad099ef8af597c5e7effb07c
Dobando-sys <obandodesart@gmail.com>
2025-04-30 20:03:27 -0500

    Update connection.py

--- connection.py
+++ connection.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import chardet
+from pathlib import Path
 import os
 
 class ConectarBase:
@@ -10,35 +11,31 @@ class ConectarBase:
         try:
             encod = self.detectar_encoding()
             base = pd.read_csv(self.csvDoc, sep=';', encoding=encod, low_memory=False)
-            base = base.dropna()
-            
+            base = base.dropna()  # Elimina filas con valores faltantes
             return pd.DataFrame(base)
         
         except Exception as e:
             print(f"Error al cargar la base: {e}")
             return pd.DataFrame()  # Retorna un DataFrame vacío si algo falla
 
-    
     def detectar_encoding(self):
         with open(self.csvDoc, 'rb') as f:
-            resultado = chardet.detect(f.read(100000))  # Lee los primeros 100KB
-        
+            resultado = chardet.detect(f.read(100000)) 
         return resultado['encoding']
     
-    def imprimir(self):
+    #Imprime las primeras filas de cada DataFrame en la carpeta "bases"
+    def imprimirHead(self):
         df = self.traer()
-        print(df.head())
+        if df.empty:
+            print("El DataFrame está vacío o hubo un error al cargar los datos.")
+        else:
+            print(df.head())  # Muestra las primeras filas del DataFrame
 
-# Definición de las bases de datos
-# Se definen las bases de datos a utilizar, junto con sus rutas locales (Bases en csv) y URLs de consulta en linea.
-bases ={"Enfermedades Cronicas en Bucaramanga": ["C:/Users/esteban.obando/OneDrive - SEMILLAS ELITE DE PALMA PARA LAS AMÉRICAS S.A.S/Documentos/GitHub/DiabAlert/bases/Enfermedades Cronicas Bucaramanga.csv",
-                                                "https://www.datos.gov.co/Salud-y-Protecci-n-Social/39-Enfermedades-cr-nicas-en-el-municipio-de-Bucara/4iz7-suhz/about_data"],
-        
-        "Muertes Prematuras por Diabetes en Bogota":["C:/Users/esteban.obando/OneDrive - SEMILLAS ELITE DE PALMA PARA LAS AMÉRICAS S.A.S/Documentos/GitHub/DiabAlert/bases/Muertes Prematuras por Diabetes.csv",
-                                                    "https://datosabiertos.bogota.gov.co/dataset/tasa-de-mortalidad-por-diabetes-mellitus-bogota-d-c/resource/43d6df80-8661-4b6d-b29a-f1e5f17e59d5?inner_span=True"],
-        
-        "Diabetes Mellitus no insulinodependiente":["C:/Users/esteban.obando/OneDrive - SEMILLAS ELITE DE PALMA PARA LAS AMÉRICAS S.A.S/Documentos/GitHub/DiabAlert/bases/Diabetes Mellitus no insulinodependiente.csv",
-                                                "https://www.datos.gov.co/Salud-y-Protecci-n-Social/DIABETES-MELLITUS-NO-INSULINODEPENDIENTE/nf9g-8bes/about_data"]}
 
-base = ConectarBase(bases["Diabetes Mellitus no insulinodependiente"][0])
-base.imprimir()
+if __name__ == "__main__":
+    carpeta = Path(os.path.dirname(os.path.abspath(__file__)))
+    for archivo_csv in carpeta.glob('*.csv'):
+        baseName = ConectarBase(archivo_csv)
+        print(f"Archivo: {archivo_csv.name}:")
+        baseName.imprimirHead() 
+        print()
